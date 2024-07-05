58b2a2fb1d4fa3e1c698a8f44f01ffb6241f5543
Arif Rahman Hakim <new.arifrh199@gmail.com>
2024-07-05 17:22:02 +0800

    feat(activity): add type in activity

--- src/model/activity.ts
+++ src/model/activity.ts
@@ -1,8 +1,11 @@
 import { Team } from "@/model/user";
 
+export type TypeActivity = "ENUMERATION" | "SUPERVISION" | "PROCESSING";
+
 export type Activity = {
   name: string;
   code: string;
   unit: string;
+  type: TypeActivity;
   team: Team;
 };
--- src/schema/activity.ts
+++ src/schema/activity.ts
@@ -6,6 +6,11 @@ export const activitySchema = new Schema<Activity>(
     name: { type: String, required: true },
     code: { type: String, required: true },
     unit: { type: String, required: true },
+    type: {
+      type: String,
+      required: true,
+      enum: ["ENUMERATION", "SUPERVISION", "PROCESSING"],
+    },
     team: {
       type: String,
       required: true,
